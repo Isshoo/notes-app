9af24197bedf77473cf5d897dfde15af99ceea0e
Tods <algy25ng@gmail.com>
2024-09-13 06:22:54 +0800

    revisi newNotes(id)

--- src/script/utility/utils.js
+++ src/script/utility/utils.js
@@ -1,12 +1,23 @@
 class Utils {
   static generateUniqueId() {
-    const unikId = Math.floor(Math.random() * 10000000000).toString(16);
-    return `notes-${unikId}-${unikId}-${unikId}`;
+    const unikId1 = Math.floor(Math.random() * 10000000000)
+      .toString(16)
+      .padStart(4, "0")
+      .slice(0, 4);
+    const unikId2 = Math.floor(Math.random() * 10000000000)
+      .toString(16)
+      .padStart(4, "0")
+      .slice(0, 4);
+    const unikId3 = Math.floor(Math.random() * 10000000000)
+      .toString(16)
+      .padStart(4, "0")
+      .slice(0, 4);
+    return `notes-${unikId1}-${unikId2}-${unikId3}`;
   }
 
   static generateCreatedAt() {
     const date = new Date();
-    return date;
+    return date.toISOString();
   }
 
   static makeNewNote(id, title, body, createdAt, archived) {
